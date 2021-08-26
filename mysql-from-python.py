6e011b2bf3a26a8594afb809d16a8bc33fc9ab5a
Tom1358 <thomasellis358@hotmail.com>
2021-08-26 19:41:59 +0000

    Open connection to database, use that connection to create a cursor - allowing us to interact with the database - and use DictCursor to print all rows in the Artist table

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -12,12 +12,11 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a query
-    with connection.cursor() as cursor:
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
         sql = "SELECT * FROM Artist;"
         cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        for row in cursor:
+            print(row)
 finally:
     # close connection, regardless of whether the above was successful
     connection.close()
-    
