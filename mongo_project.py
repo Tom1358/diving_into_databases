a85b63fb8e123949d743f2fdc7b9fbef3842e0ac
Tom1358 <thomasellis358@hotmail.com>
2021-08-30 19:26:00 +0000

    Tested find_record() function (option 2) to confirm working

--- mongo_project.py
+++ mongo_project.py
@@ -16,6 +16,7 @@ def mongo_connect(url):
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
 
+
 def show_menu():
     print("")
     print("1. Add a record")
@@ -73,13 +74,22 @@ def add_record():
         print("Error accessing database")
 
 
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+                
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
             add_record()
         elif option == "2":
-            print("You have selected option 2")
+            find_record()
         elif option == "3":
             print("You have selected option 3")
         elif option == "4":
